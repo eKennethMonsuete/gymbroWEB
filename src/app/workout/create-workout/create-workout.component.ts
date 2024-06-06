88ed1b81d08a7b535886211ccb657921f20dfc15
Kenneth <keeentt19@gmail.com>
2024-06-06 18:56:16 -0300

    update work funciona

--- src/app/workout/create-workout/create-workout.component.ts
+++ src/app/workout/create-workout/create-workout.component.ts
@@ -88,4 +88,7 @@ export class CreateWorkoutComponent implements OnInit {
 
 
 
+
+
+
 }
--- src/app/workout/workout-update/workout-update.component.ts
+++ src/app/workout/workout-update/workout-update.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Workout } from './Workout';
 import { ActivatedRoute,  Router } from '@angular/router';
 import { WorkoutUpdateService } from './workout-update.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-workout-update',
@@ -11,29 +12,51 @@ import { WorkoutUpdateService } from './workout-update.service';
 })
 export class WorkoutUpdateComponent implements OnInit {
 
-  workout : Workout = {
+  formWorkoutRegister! : FormGroup;
 
-    id : 0,
-    workout1 : "",
-    workout2 : "",
-    note : "",
-    name : "",
-    socialMedia : "" }
+  charCount = 0;
+  charCount2 = 0;
+  charCount3 = 0;
 
   constructor( private workoutUpdateService : WorkoutUpdateService,
     private route : ActivatedRoute,
     private router : Router
-  ) { }
+  ) {
+    this.formWorkoutRegister = new FormGroup({
+      id : new FormControl(),
+      workout1 : new FormControl('', [Validators.maxLength(255)]),
+      workout2 : new FormControl('', [Validators.maxLength(255)]),
+      note : new FormControl('', [Validators.maxLength(255)]),
+      name : new FormControl('', [Validators.required]),
+      socialMedia : new FormControl('', [Validators.required])
+    })
+   }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-        this.workoutUpdateService.findWorkoutById(parseInt(id!)).subscribe((workout) =>{
-              this.workout = workout;
-  })
+    if (id) {
+      this.workoutUpdateService.findWorkoutById(parseInt(id)).subscribe((workout: Workout) => {
+        this.formWorkoutRegister.patchValue(workout);
+      });
+    }
+
+
+
 
 }
   updateWorkout(){
-    this.workoutUpdateService.updateWorkout(this.workout.id, this.workout).subscribe(
+    if(this.formWorkoutRegister.valid){
+      const formValues = this.formWorkoutRegister.value
+      const workout : Workout = {
+        id: formValues.id,
+        workout1: formValues.workout1,
+        workout2: formValues.workout2,
+        note : formValues.note,
+        name : formValues.name,
+        socialMedia : formValues.socialMedia
+      }
+
+      this.workoutUpdateService.updateWorkout(workout.id, workout).subscribe(
       resposta => {
         alert("treino atualizado")
         this.router.navigate(['workout']);
@@ -41,9 +64,25 @@ export class WorkoutUpdateComponent implements OnInit {
       }, error =>{
         console.log(error)
       })
+
+    }
   }
 
   cancel(){
     this.router.navigate(['workout']);
   }
+
+  onTextChanged(event: any) {
+    this.charCount = event.target.value.length;
+  }
+  onTextChanged2(event: any) {
+    this.charCount2 = event.target.value.length;
+
+  }
+  onTextChanged3(event: any) {
+    this.charCount3 = event.target.value.length;
+
+  }
+
+
 }
